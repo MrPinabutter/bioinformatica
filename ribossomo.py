0859031349596c6582d1ddf89a15c34308166fb6
Vitor Rafael Rocha Campelo <vitorafael.r.c@gmail.com>
2021-04-08 15:32:51 -0300

    🔬 Start and stop codon

--- ribossomo.py
+++ ribossomo.py
@@ -1,7 +1,12 @@
 from amino import aminos
 
 rna_m = input().replace('.', '') # Remove o caractere .
-
+start = False
 for n in range(0, len(rna_m)-1, 3): # Pula 3 bases para formar as trincas
     trinca = rna_m[n:n+3]
-    print(aminos[trinca]) 
+    if 'Start' in aminos[trinca]:
+        start = True
+    if start:
+        print(aminos[trinca])
+        if 'Stop' in trinca:
+            break
